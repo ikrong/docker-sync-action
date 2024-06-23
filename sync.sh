3b785eb6064f43a51ef799f752ec39d5973068ec
krong <contact@ikrong.com>
2024-06-23 13:12:45 +0800

    upgrade skopeo (#1)

--- sync.sh
+++ sync.sh
@@ -27,7 +27,7 @@ function run_with_lines() {
 function sync() {
     echo "::group::Syncing $1"
     arr=($1 )
-    skopeo sync -a --src docker --dest docker "$SOURCE/${arr[0]}" "$DESTINATION/${arr[1]}"
+    skopeo --debug sync --multi-arch all --src docker --dest docker "$SOURCE/${arr[0]}" "$DESTINATION/${arr[1]}"
     # 判断是否执行成功
     if [ $? -ne 0 ]; then
         echo "::error::Syncing $1 failed"
@@ -40,7 +40,7 @@ function sync() {
 function copy() {
     echo "::group::Coping $1"
     arr=($1 )
-    skopeo copy -a "docker://$SOURCE/${arr[0]}" "docker://$DESTINATION/${arr[1]}"
+    skopeo --debug copy --multi-arch all "docker://$SOURCE/${arr[0]}" "docker://$DESTINATION/${arr[1]}"
     # 判断是否执行成功
     if [ $? -ne 0 ]; then
         echo "::error::Coping $1 failed"
@@ -49,6 +49,20 @@ function copy() {
     fi
     echo "::endgroup::"
 }
+
+function install_latest_skopeo() {
+    echo "::group::Install Skopeo"
+    echo 'deb http://download.opensuse.org/repositories/home:/alvistack/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:alvistack.list
+    curl -fsSL https://download.opensuse.org/repositories/home:alvistack/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_alvistack.gpg > /dev/null
+    sudo apt update
+    sudo apt -o Dpkg::Options::="--force-overwrite" install skopeo
+    echo "::endgroup::"
+}
+
+install_latest_skopeo
+
+docker -v
+skopeo -v
 
 echo "::group::Login"
 login $SOURCE $SOURCE_CREDENTIAL
